f0ca456ed4b2906281fc419129476b075831f41b
stas <stas.v.nebyshinets@gmail.com>
2021-03-02 19:06:00 +0300

    feat: add redirection by click with speed calculation

--- src/bsu/rfe/java/group9/lab5/Nebyshinets/varC/Field.java
+++ src/bsu/rfe/java/group9/lab5/Nebyshinets/varC/Field.java
@@ -1,74 +1,113 @@
 package bsu.rfe.java.group9.lab5.Nebyshinets.varC;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 @SuppressWarnings("serial")
 public class Field extends JPanel {
-    // Флаг приостановленности движения
     private boolean paused;
-    // Динамический список скачущих мячей
     private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
-    // Класс таймер отвечает за регулярную генерацию событий ActionEvent
-// При создании его экземпляра используется анонимный класс,
-// реализующий интерфейс ActionListener
+    private double startX;
+    private double startY;
+    private long startTime;
+    private BouncingBall currentBall = null;
+    private MainFrame outerFrame;
+
     private Timer repaintTimer = new Timer(10, new ActionListener() {
         public void actionPerformed(ActionEvent ev) {
-// Задача обработчика события ActionEvent - перерисовка окна
             repaint();
         }
     });
-    // Конструктор класса BouncingBall
-    public Field() {
-// Установить цвет заднего фона белым
+
+    public Field(MainFrame outer) {
         setBackground(Color.WHITE);
-// Запустить таймер
+        this.addMouseListener(new Field.MouseHandler());
+        outerFrame = outer;
         repaintTimer.start();
     }
-    // Унаследованный от JPanel метод перерисовки компонента
+
     public void paintComponent(Graphics g) {
-// Вызвать версию метода, унаследованную от предка
         super.paintComponent(g);
         Graphics2D canvas = (Graphics2D) g;
-// Последовательно запросить прорисовку от всех мячей из списка
         for (BouncingBall ball: balls) {
             ball.paint(canvas);
         }
     }
-    // Метод добавления нового мяча в список
+
     public void addBall() {
-//Заключается в добавлении в список нового экземпляра BouncingBall
-// Всю инициализацию положения, скорости, размера, цвета
-// BouncingBall выполняет сам в конструкторе
         balls.add(new BouncingBall(this));
     }
-    // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
+
     public synchronized void pause() {
-// Включить режим паузы
         paused = true;
     }
-    // Метод синхронизированный, т.е. только один поток может
-// одновременно быть внутри
+
     public synchronized void resume() {
-// Выключить режим паузы
         paused = false;
-// Будим все ожидающие продолжения потоки
         notifyAll();
     }
-    // Синхронизированный метод проверки, может ли мяч двигаться
-// (не включен ли режим паузы?)
+
     public synchronized void canMove(BouncingBall ball) throws
             InterruptedException {
         if (paused) {
-// Если режим паузы включен, то поток, зашедший
-// внутрь данного метода, засыпает
             wait();
+        }
+    }
+
+    public class MouseHandler extends MouseAdapter {
+        public MouseHandler() {
+        }
+
+        public void mouseClicked(MouseEvent ev) {
+        }
+
+        public void mousePressed(MouseEvent ev) {
+            if (ev.getButton() == 1) {
+                pause();
+                startX = ev.getX();
+                startY = ev.getY();
+                startTime = System.currentTimeMillis();
+                for( int i = 0; i < balls.size(); i++ ){
+                    BouncingBall ball = balls.get(i);
+                    if( Math.pow(startX - ball.getX(), 2) + Math.pow(startY - ball.getY(), 2) < Math.pow(ball.getRadius(), 2)){
+                        currentBall = ball;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void mouseReleased(MouseEvent ev) {
+            if (ev.getButton() == 1) {
+                if(currentBall != null) {
+                    double deltaX = ev.getX() - startX;
+                    double deltaY = ev.getY() - startY;
+                    long deltaTime = System.currentTimeMillis() - startTime;
+
+                    double angle = Math.atan( deltaY/deltaX );
+                    if(deltaX < 0){
+                        if(deltaY > 0){
+                            angle += Math.PI;
+                        } else {
+                            angle -= Math.PI;
+                        }
+                    }
+
+                    int newSpeed = 3*(int)Math.sqrt( deltaX*deltaX + deltaY*deltaY )/(int)deltaTime;
+
+                    currentBall.setSpeed(newSpeed, angle);
+
+                    currentBall = null;
+                }
+                if(outerFrame.getPaused()) {
+                    resume();
+                }
+            }
         }
     }
 }
